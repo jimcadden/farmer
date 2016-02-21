173cb1ec6c3d144df045a6a25ed0a93ab9b59a44
Jim Cadden <jmcadden@bu.edu>
2016-02-20 22:58:26 -0500

    poc: network config

--- misc/poc/main.go
+++ misc/poc/main.go
@@ -23,7 +23,6 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
-	//	"strings"
 
 	"github.com/jmcadden/circuit/client"
 )
@@ -51,11 +50,12 @@ func main() {
 		println("usage: poc circuit://...")
 		os.Exit(1)
 	}
-	println("circuit: dialing into", os.Args[1])
+
+	// connect to circuit network
 	c := client.Dial(os.Args[1], nil)
 
 	// check for prereqs
-	prereqs := []string{"brctl", "qemu-system-x86_64", "dnsmasq", "tunctl"}
+	prereqs := []string{"route", "brctl", "qemu-system-x86_64", "dnsmasq", "tunctl", "ip"}
 	servers := []string{}
 	for _, r := range c.View() {
 		err := check_prereqs(r, prereqs)
@@ -66,10 +66,55 @@ func main() {
 	if len(servers) > 0 {
 		abort("following servers missing prereqs: %v", servers)
 	}
-
 	debug("prereq check complete")
-	// set up remote networks
+	// set up networks
+	count := 0
+	for _, r := range c.View() {
+		count += 1
+		err := setup_network(r, count)
+		if err {
+			abort("[%s] network setup error: %v", r.ServerID(), err)
+		}
+	}
+	debug("network setup complete")
+	//
+	//
+}
 
+func setup_network(host client.Anchor, id int) bool {
+
+	//sid := host.ServerID()
+	// ids
+	ip_base := "10.0.0." + strconv.Itoa(id)
+	//ip_new := "10.0.1." + strconv.Itoa(id)
+	eth0 := "eth0"
+	eth00 := "eth0.1"
+	br0 := "br_poc"
+	tap0 := "tap_poc"
+	orquit := " || exit 1"
+	// commands
+	commands := []string{}
+	commands = append(commands, "ip link add link "+eth0+" "+eth00+" type vlan id 1")
+	commands = append(commands, "ip link set "+eth00+" up")
+	commands = append(commands, "brctl addbr "+br0)
+	commands = append(commands, "ip link set "+br0+" up")
+	commands = append(commands, "ip addr add "+ip_base+"/24 dev "+br0)
+	commands = append(commands, "ip tuntap add dev "+tap0+" mode tap multi_queue")
+	//commands = append(commands, "ip addr add "+ip_new+"/24 dev "+tap0)
+	commands = append(commands, "ip link set "+tap0+" up")
+	commands = append(commands, "brctl addif "+br0+" "+tap0+" "+eth00)
+
+	for _, v := range commands {
+		cmd := v + orquit
+		out, err := runShell(host, cmd)
+		debug("[%s] "+cmd, host.ServerID())
+		if err != nil {
+			debug("[%s] %s ", host.ServerID(), err)
+		} else {
+			debug("[%s] "+out, host.ServerID())
+		}
+	}
+	return false
 }
 
 // runShell executes the shell command on the given host,
